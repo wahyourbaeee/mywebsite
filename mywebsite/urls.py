91cde199ec05226425b487c79a257806fd764e75
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-04-10 22:23:05 +0700

    perbaikan pada urls utama

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from mywebsite import settings
-from . import views
-
-from django.conf import settings # 
-from django.conf.urls.static import static 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
-    path('blog/', include('blog.urls')),
-    path('about/', include('about.urls')),
-    path('contact/', include('contact.urls')),
-    path('projects/', include('projects.urls')),
+    path('', include('main_app.urls')), # Pastikan 'main_app' sudah terdaftar di INSTALLED_APPS
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
